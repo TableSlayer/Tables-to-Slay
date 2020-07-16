15a27ef5b12f0d4638d3211b24f915fce6d97bfe
Sidharth Nambiar <60028950+TableSlayer@users.noreply.github.com>
2020-07-16 17:18:53 +0530

    Add files via upload

---
+++ precache-manifest.f2fc692e1ecdb530485bd718d53840c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e59f960496a47ab72c77cd6a7aa5c8e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51c59c7e14711ce60702",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "07687f18feb9712126a4",
+    "url": "/static/js/2.9b959b1a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9b959b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51c59c7e14711ce60702",
+    "url": "/static/js/main.d40d3e31.chunk.js"
+  },
+  {
+    "revision": "74c40a6c43ff3645c69b",
+    "url": "/static/js/runtime-main.5d9f296c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
